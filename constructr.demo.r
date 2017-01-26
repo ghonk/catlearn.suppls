26e04cc85a61ef1af93d00728481eca4936b1485
nolan <nolanbconaway@gmail.com>
2017-01-26 17:07:55 -0600

    constructr drafted

--- constructr.demo.r
+++ constructr.demo.r
@@ -1,6 +1,4 @@
 
-
-
 # # # load model functions
 source('utils.r')
 source('constructr.r')
@@ -13,13 +11,14 @@ cases <- demo_cats(2)
 # make a single presentation order with a test set after training
 tr <- tr_init( dim(inputs)[1] )
 tr <- tr_add(inputs, tr, labels = labels, blocks = 2, 
-    ctrl = 0, shuffle = TRUE, reset = TRUE)
+	ctrl = 0, shuffle = TRUE, reset = TRUE)
 tr <- tr_add(inputs, tr, ctrl = 2)
 print(tr)
 
 # And you can just add another model's presentation order on there!
 tr <- tr_add(inputs, tr, labels = labels, blocks = 2, 
-    ctrl = 0, shuffle = TRUE, reset = TRUE)
+	ctrl = 0, shuffle = TRUE, reset = TRUE)
 tr <- tr_add(inputs, tr, ctrl = 2)
 print(tr)
+
 
