19b1ccfbb1a6b70fd98d6e354223a72e28b56560
garrett <garretthonke@gmail.com>
2017-09-20 01:51:57 -0400

    added medin and schwanenflugal 81 data

--- R/catlearn_suppls.r
+++ R/catlearn_suppls.r
@@ -164,6 +164,8 @@ generate_state <- function(input, categories, colskip, continuous, make_wts,
 #'     \item \code{unifr} Unidimensional rule plus family resemblance structure
 #'     \item \code{unfr1}, \code{unfr2} UNI-FR (separated for autoencoder modeling)
 #'     \item \code{fr4d} Four-dimensional family resemblance structure
+#'     \item \code{mns81_ls} Medin and Schwanenflugel '81 Linearly Separable Structure
+#'     \item \code{mns81_nls} Medin and Schwanenflugel '81 Non-Linearly Separable Structure
 #'     \item \code{type1}, \code{type2}, \code{typeN}... Shepard, Hovland and Jenkin's elemental types
 #'     \item \code{multiclass} 4 class problem built from SHJ Type II
 #'     }
@@ -215,6 +217,28 @@ get_test_inputs <- function(target_cats){
       1, -1,  1,  1,
      -1,  1,  1,  1),  ncol = 4, byrow = TRUE),
       labels = c(1, 1, 1, 1, 1, 2, 2, 2, 2, 2)),
+
+      mns81_ls = list(ins = matrix(c(
+      1, -1,  1,  1,
+      1, -1,  1, -1,
+      1,  1, -1,  1,
+     -1,  1,  1, -1,
+      1, -1, -1,  1,
+     -1, -1,  1, -1,
+     -1,  1, -1, -1,
+     -1, -1, -1,  1),  ncol = 4, byrow = TRUE),
+      labels = c(1, 1, 1, 1, 2, 2, 2, 2)),
+
+      mns81_nls = list(ins = matrix(c(
+      1, -1, -1, -1,
+     -1,  1,  1,  1,
+      1,  1,  1, -1,
+      1, -1,  1,  1,
+     -1,  1,  1, -1,
+      1, -1, -1,  1,
+     -1, -1, -1, -1,
+     -1, -1, -1,  1),  ncol = 4, byrow = TRUE),
+      labels = c(1, 1, 1, 1, 2, 2, 2, 2)),
 
       type1 = list(ins = matrix(c(
      -1, -1, -1,
