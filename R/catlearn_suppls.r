fa49c45182924da2d8727addd2f1b2cb05f1b196
garrett <garretthonke@gmail.com>
2017-10-04 14:48:01 -0400

    added medin and schwanenflugel 81 experiment 2 category structure

--- R/catlearn_suppls.r
+++ R/catlearn_suppls.r
@@ -164,8 +164,10 @@ generate_state <- function(input, categories, colskip, continuous, make_wts,
 #'     \item \code{unifr} Unidimensional rule plus family resemblance structure
 #'     \item \code{unfr1}, \code{unfr2} UNI-FR (separated for autoencoder modeling)
 #'     \item \code{fr4d} Four-dimensional family resemblance structure
-#'     \item \code{mns81_ls} Medin and Schwanenflugel '81 Linearly Separable Structure
-#'     \item \code{mns81_nls} Medin and Schwanenflugel '81 Non-Linearly Separable Structure
+#'     \item \code{mns81e2_ls} Medin and Schwanenflugel '81 Experiment 2 Linearly Separable Structure
+#'     \item \code{mns81e2_nls} Medin and Schwanenflugel '81 Experiment 2 Non-Linearly Separable Structure
+#'     \item \code{mns81e4_ls} Medin and Schwanenflugel '81 Experiment 4 Linearly Separable Structure
+#'     \item \code{mns81e4_nls} Medin and Schwanenflugel '81 Experiment 4 Non-Linearly Separable Structure
 #'     \item \code{type1}, \code{type2}, \code{typeN}... Shepard, Hovland and Jenkin's elemental types
 #'     \item \code{multiclass} 4 class problem built from SHJ Type II
 #'     }
@@ -218,7 +220,36 @@ get_test_inputs <- function(target_cats){
      -1,  1,  1,  1),  ncol = 4, byrow = TRUE),
       labels = c(1, 1, 1, 1, 1, 2, 2, 2, 2, 2)),
 
-      mns81_ls = list(ins = matrix(c(
+      mns81e2_ls = list(ins = matrix(c(
+     -1, -1,  1,
+     -1,  1, -1,
+      1, -1, -1,
+      1,  1, -1,
+      1, -1,  1,
+     -1,  1,  1),  ncol = 3, byrow = TRUE),
+      labels = c(1, 1, 1, 2, 2, 2)),
+
+      mns81e2_nls = list(ins = matrix(c(
+     -1, -1,  1,
+     -1,  1,  1,
+      1, -1, -1,
+      1,  1, -1,
+      1, -1,  1,
+     -1,  1, -1),  ncol = 3, byrow = TRUE),
+      labels = c(1, 1, 1, 2, 2, 2)),
+
+      mns81e4_nls = list(ins = matrix(c(
+      1, -1, -1, -1,
+     -1,  1,  1,  1,
+      1,  1,  1, -1,
+      1, -1,  1,  1,
+     -1,  1,  1, -1,
+      1, -1, -1,  1,
+     -1, -1, -1, -1,
+     -1, -1, -1,  1),  ncol = 4, byrow = TRUE),
+      labels = c(1, 1, 1, 1, 2, 2, 2, 2)),
+
+      mns81e4_ls = list(ins = matrix(c(
       1, -1,  1,  1,
       1, -1,  1, -1,
       1,  1, -1,  1,
@@ -229,7 +260,7 @@ get_test_inputs <- function(target_cats){
      -1, -1, -1,  1),  ncol = 4, byrow = TRUE),
       labels = c(1, 1, 1, 1, 2, 2, 2, 2)),
 
-      mns81_nls = list(ins = matrix(c(
+      mns81e4_nls = list(ins = matrix(c(
       1, -1, -1, -1,
      -1,  1,  1,  1,
       1,  1,  1, -1,
